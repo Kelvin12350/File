83704f2500e6182e5ea1dd09f0ccf7f44207a493
Heatless <dianwilson66@gmail.com>
2025-09-22 06:30:20 +0100

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,13 @@
+{
+  "name": "mega-project",
+  "version": "1.0.0",
+  "description": "MEGA project deployment",
+  "scripts": {
+    "start": "vercel dev"
+  },
+  "author": "Your Name",
+  "license": "MIT",
+  "devDependencies": {
+    "vercel": "^32.0.0"
+  }
+      }
